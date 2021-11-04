2ad0630c5fe9e297aeddb3a132d5aed138d67372
chamsonxie <chamson1995@qq.com>
2021-11-04 17:37:24 +0800

    Create answers.js

---
+++ answers.js
@@ -0,0 +1,900 @@
+export const ANSWER = [
+  {
+    china:'需要一个相当大的努力',
+    english:'A Substantial Effort Will Be Required'
+  },
+  {
+    china:'从现在开始 一年也没有关系',
+    english:'A Year From Now It Won\'t Matter'
+  },
+  {
+    china:'绝对不是',
+    english:'Absolutely Not'
+  },
+  {
+    china:'接受改变你的日常行为',
+    english:'Accept A Change To Your Routine'
+  },
+  {
+    china:'似乎已经是真实的了',
+    english:'Act As Though It Is Already Real'
+  },
+  {
+    china:'采用一个冒险的态度',
+    english:'Adopt An Adventurous Attitude'
+  },
+  {
+    china:'允许你选择先休息一下',
+    english:'Allow Yoursele To Rest First'
+  },
+  {
+    china:'小心谨慎的靠近',
+    english:'Approach Cautiously'
+  },
+  {
+    china:'你还是问问爸爸吧',
+    english:'Ask Your Father'
+  },
+  {
+    china:'你还是问问妈妈吧',
+    english:'Ask Your Mother'
+  },
+  {
+    china:'援助会让你成功进步',
+    english:'Assistance Would Make Your Progress A Success'
+  },
+  {
+    china:'避免第一个解决方案',
+    english:'Avoid The First Solution'
+  },
+  {
+    china:'被人肯定',
+    english:'Be Delightfully Sure Of It'
+  },
+  {
+    china:'更慷慨',
+    english:'Be More Generous'
+  },
+  {
+    china:'耐心点',
+    english:'Be Patient'
+  },
+  {
+    china:'实际一点吧',
+    english:'Be Practical'
+  },
+  {
+    china:'打赌',
+    english:'Bet On It'
+  },
+  {
+    china:'耐心的等待',
+    english:'Better To Wait'
+  },
+  {
+    china:'合作将会是关键',
+    english:'Collaboration Will Be The Key'
+  },
+  {
+    china:'考虑一下这个机会',
+    english:'Consider It An Opportunity'
+  },
+  {
+    china:'数到10；再问',
+    english:'Count To 10; Ask Again'
+  },
+  {
+    china:'以后处理',
+    english:'Deal With It Later'
+  },
+  {
+    china:'当然',
+    english:'Definitely'
+  },
+  {
+    china:'早点做到这一点',
+    english:'Do It Early'
+  },
+  {
+    china:'在这个时候不要问太多了',
+    english:'Don\'t Ask For Any More At This Time'
+  },
+  {
+    china:'不要担心',
+    english:'Don\'t Be Concerned'
+  },
+  {
+    china:'不要迫于压力太快',
+    english:'Don\'t Be Pressured Into Acting Too Quickly'
+  },
+  {
+    china:'别傻了',
+    english:'Don\'t Be Ridiculous'
+  },
+  {
+    china:'不要赌',
+    english:'Don\'t Bet On It'
+  },
+  {
+    china:'不用怀疑了',
+    english:'Don\'t Doubt It'
+  },
+  {
+    china:'不要忘记有乐趣',
+    english:'Don\'t Forget To Have Fun'
+  },
+  {
+    china:'不要陷入你的感情',
+    english:'Don\'t Get Caught Up In Your Emotions'
+  },
+  {
+    china:'不要犹豫',
+    english:'Don\'t Hesitate'
+  },
+  {
+    china:'不要忽视显而易见的',
+    english:'Don\'t Ignore The Obvious'
+  },
+  {
+    china:'不要过分',
+    english:'Don\'t Overdo It'
+  },
+  {
+    china:'不要等待',
+    english:'Don\'t Wait'
+  },
+  {
+    china:'别浪费时间了',
+    english:'Don\'t Waste Your Time'
+  },
+  {
+    china:'怀疑',
+    english:'Doubt It'
+  },
+  {
+    china:'尽情体验',
+    english:'Enjoy The Experience'
+  },
+  {
+    china:'要解决',
+    english:'Expect To Settle'
+  },
+  {
+    china:'探讨其俏皮的好奇心',
+    english:'Explore It With Playful Curiosity'
+  },
+  {
+    china:'完成其他事情的第一',
+    english:'Finish Something Else First'
+  },
+  {
+    china:'专注你的家庭生活',
+    english:'Focus On Your Home Life'
+  },
+  {
+    china:'遵循别人的引导',
+    english:'Follow Someone Else\'s Lead'
+  },
+  {
+    china:'遵循专家的建议',
+    english:'Follow The Advice Of Experts'
+  },
+  {
+    china:'履行你自己的义务',
+    english:'Follow Through On Your Obligations'
+  },
+  {
+    china:'按照你的意愿',
+    english:'Follow Through With Your Good Intentions'
+  },
+  {
+    china:'温柔的坚持就是胜利',
+    english:'Gentle Persistence Will Pay Off'
+  },
+  {
+    china:'获得更清晰的视野',
+    english:'Get A Clearer View'
+  },
+  {
+    china:'把它写下来',
+    english:'Get It In Writing'
+  },
+  {
+    china:'给它你的一切',
+    english:'Give It All You\'ve Got'
+  },
+  {
+    china:'如果是，做的很好；如果不是，就不要这么做；',
+    english:'If It\'s Done Well; If Not, Don\'t Do It At All'
+  },
+  {
+    china:'如果你按我说的做',
+    english:'If You Do As You\'re Told'
+  },
+  {
+    china:'如果你不反抗',
+    english:'If You Don\'t Resist'
+  },
+  {
+    china:'调查研究，然后享受它！',
+    english:'Investigate And Then Enjoy It'
+  },
+  {
+    china:'这不可能失败',
+    english:'It Cannot Fail'
+  },
+  {
+    china:'这可能是特别的',
+    english:'It Could Be Extraordinary'
+  },
+  {
+    china:'这是肯定的',
+    english:'It Is Certain'
+  },
+  {
+    china:'这不是重要的',
+    english:'It Is Not Significant'
+  },
+  {
+    china:'这是非常重要',
+    english:'It Is Significant'
+  },
+  {
+    china:'这是你不会忘记的',
+    english:'It Is Something You Won\'t Forget'
+  },
+  {
+    china:'这肯定会让事情变得有趣',
+    english:'It Is Sure To Make Things Interesting'
+  },
+  {
+    china:'这是不确定的',
+    english:'It Is Uncertain'
+  },
+  {
+    china:'是值得的麻烦',
+    english:'It Is Worth The Trouble'
+  },
+  {
+    china:'这可能已经是木已成舟',
+    english:'It May Already Be A Done Deal'
+  },
+  {
+    china:'这可能很难，但你会发现它的价值',
+    english:'It May Be Difficult But You Will Find Value In It'
+  },
+  {
+    china:'这个似乎放心',
+    english:'It Seems Assured'
+  },
+  {
+    china:'这将影响别人看你',
+    english:'It Will Affect How Others See You'
+  },
+  {
+    china:'这将是一种享受',
+    english:'It Will Be A Pleasure'
+  },
+  {
+    china:'这会带来好运',
+    english:'It Will Bring Good Luck'
+  },
+  {
+    china:'这将会创造一个扰乱',
+    english:'It Will Create A Stir'
+  },
+  {
+    china:'这仍然是不可预测的',
+    english:'It Will Remain Unpredictable'
+  },
+  {
+    china:'它会支持你的',
+    english:'It Will Sustain You'
+  },
+  {
+    china:'这将更好地专注于自己的工作',
+    english:'It Would Be Better To Focus On Your Work'
+  },
+  {
+    china:'这是不明智的',
+    english:'It Would Be Inadvisable'
+  },
+  {
+    china:'它会让你付出代价',
+    english:'It\'ll Cost You'
+  },
+  {
+    china:'这是一个不错的时间安排',
+    english:'It\'s A Good Time To Make Plans'
+  },
+  {
+    china:'会很棒的',
+    english:'It\'s Gonna Be Great'
+  },
+  {
+    china:'这是不值得的斗争',
+    english:'It\'s Not Worth A Struggle'
+  },
+  {
+    china:'是你走的时候',
+    english:'It\'s Time For You To Go'
+  },
+  {
+    china:'保持开放的心态',
+    english:'Keep An Open Mind'
+  },
+  {
+    china:'不要让别人知道',
+    english:'Keep It To Yourself'
+  },
+  {
+    china:'笑一下',
+    english:'Laugh About It'
+  },
+  {
+    china:'离开旧的解决方案',
+    english:'Leave Behind Old Solutions'
+  },
+  {
+    china:'过去的事就让它过去吧',
+    english:'Let It Go'
+  },
+  {
+    china:'仔细聆听；那么你就会知道',
+    english:'Listen More Carefully; Then You Will Know'
+  },
+  {
+    china:'列出原因',
+    english:'Make A List Of Why'
+  },
+  {
+    china:'为什么不列出原因',
+    english:'Make A List Of Why Not'
+  },
+  {
+    china:'也许',
+    english:'Maybe'
+  },
+  {
+    china:'不幸的是极有可能的',
+    english:'Mishaps Are Highly Probable'
+  },
+  {
+    china:'继续前行',
+    english:'Move On'
+  },
+  {
+    china:'从来没有',
+    english:'Never'
+  },
+  {
+    china:'没有',
+    english:'No'
+  },
+  {
+    china:'不管是什么',
+    english:'No Matter What'
+  },
+  {
+    china:'如果你不孤单',
+    english:'Not If You\'re Alone'
+  },
+  {
+    china:'现在你可以',
+    english:'Now You Can'
+  },
+  {
+    china:'只做一次',
+    english:'Only Do It Once'
+  },
+  {
+    china:'其他人将取决于你的选择',
+    english:'Others Will Depend On Your Choices'
+  },
+  {
+    china:'注重细节',
+    english:'Pay Attention To The Details'
+  },
+  {
+    china:'也许，当你老了',
+    english:'Perhaps, When You\'re Older'
+  },
+  {
+    china:'为突发事件做好准备',
+    english:'Prepare For The Unexpected'
+  },
+  {
+    china:'按下关闭',
+    english:'Press For Closure'
+  },
+  {
+    china:'须以较宽松的步伐',
+    english:'Proceed At A More Relaxed Pace'
+  },
+  {
+    china:'有时候选择太多，就代表着无从选择！',
+    english:'Realize That Too Many Choices Is As Difficult As Too Few'
+  },
+  {
+    china:'重新考虑你的方法',
+    english:'Reconsider Your Approach'
+  },
+  {
+    china:'相关的问题可能的浮出水面',
+    english:'Related Issues May Surface'
+  },
+  {
+    china:'保持灵活',
+    english:'Remain Flexible'
+  },
+  {
+    china:'删除你自己的障碍物',
+    english:'Remove Your Own Obstacles'
+  },
+  {
+    china:'重新确定优先次序什么是重要的',
+    english:'Reprioritize What Is Important'
+  },
+  {
+    china:'尊重规则',
+    english:'Respect The Rules'
+  },
+  {
+    china:'保存你的精力',
+    english:'Save Your Energy'
+  },
+  {
+    china:'寻找更多的选择',
+    english:'Seek Out More Options'
+  },
+  {
+    china:'设定优先等级是一个必要的过程！',
+    english:'Setting Priorities Will Be A Necessary Part Of The Process'
+  },
+  {
+    china:'很快就会解决它',
+    english:'Settle It Soon'
+  },
+  {
+    china:'转移你的焦点',
+    english:'Shift Your Focus'
+  },
+  {
+    china:'说出来吧',
+    english:'Speak Up About It'
+  },
+  {
+    china:'令人震惊的事件可能发生的',
+    english:'Startling Events May Occur As A Result'
+  },
+  {
+    china:'冒险一试',
+    english:'Take A Chance'
+  },
+  {
+    china:'负责',
+    english:'Take Charge'
+  },
+  {
+    china:'花更多的时间来决定',
+    english:'Take More Time To Decide'
+  },
+  {
+    china:'告诉别人它对你意味着什么',
+    english:'Tell Someone What It Means To You'
+  },
+  {
+    china:'那将是浪费钱',
+    english:'That Would Be A Waste Of Money'
+  },
+  {
+    china:'那是脱离你的控制',
+    english:'That\'s Out Of Your Control'
+  },
+  {
+    china:'答案就在你的后院',
+    english:'The Answer Is In Your Backyard'
+  },
+  {
+    china:'答案可能会在另一种语言',
+    english:'The Answer May Come To You In Another Language'
+  },
+  {
+    china:'最好的解决办法可能不是明显的',
+    english:'The Best Solution May Not Be The Obvious One'
+  },
+  {
+    china:'机会不会很快再来',
+    english:'The Chance Will Not Come Again Soon'
+  },
+  {
+    china:'情况将很快发生改变',
+    english:'The Circumstances Will Change Very Quickly'
+  },
+  {
+    china:'结果会是好的',
+    english:'The Outcome Will Be Positive'
+  },
+  {
+    china:'情况不明',
+    english:'The Situation Is Unclear'
+  },
+  {
+    china:'与另一种情况有潜在的联系',
+    english:'There Is A Substantial Link To Another Situation'
+  },
+  {
+    china:'有充分的理由保持乐观',
+    english:'There Is Good Reason To Be Optimistic'
+  },
+  {
+    china:'没有保证',
+    english:'There Is No Guarantee'
+  },
+  {
+    china:'会有障碍要克服',
+    english:'There Will Be Obstacles To Overcome'
+  },
+  {
+    china:'这是一个很好的时机，来制定新计划',
+    english:'This Is A Good Time To Make A New Plan'
+  },
+  {
+    china:'为了确保能作出最佳决策，需要保持冷静',
+    english:'To Ensure The Best Decision, Be Calm'
+  },
+  {
+    china:'相信自己的直觉',
+    english:'Trust Your Intuition'
+  },
+  {
+    china:'相信你的原始思想',
+    english:'Trust Your Original Thought'
+  },
+  {
+    china:'尝试一种更不太可能的解决方案',
+    english:'Try A More Unlikely Solution'
+  },
+  {
+    china:'不宜在这个时候',
+    english:'Unfavorable At This Time'
+  },
+  {
+    china:'毫无疑问',
+    english:'Unquestionably'
+  },
+  {
+    china:'无论如何你可以提升',
+    english:'Upgrade Any Way You Can'
+  },
+  {
+    china:'运用你的想象力',
+    english:'Use Your Imagination'
+  },
+  {
+    china:'等一等',
+    english:'Wait'
+  },
+  {
+    china:'等待一个更好的机会',
+    english:'Wait For A Better Offer'
+  },
+  {
+    china:'看看会发生什么',
+    english:'Watch And See What Happens'
+  },
+  {
+    china:'注意你的节奏',
+    english:'Watch Your Step As You Go'
+  },
+  {
+    china:'不管你做什么，结果将持久',
+    english:'Whatever You Do The Results Will Be Lasting'
+  },
+  {
+    china:'可以',
+    english:'Yes'
+  },
+  {
+    china:'是的，但不要强迫',
+    english:'Yes,But Don\'t Force It'
+  },
+  {
+    china:'你一定有支持',
+    english:'You Are Sure To Have Support'
+  },
+  {
+    china:'你太近的看了',
+    english:'You Are Too Close To See'
+  },
+  {
+    china:'你可能觉得自己无法妥协',
+    english:'You Could Find Yourself Unable To Compromise'
+  },
+  {
+    china:'你真的不在乎',
+    english:'You Don\'t Really Care'
+  },
+  {
+    china:'你知道现在比以前更好',
+    english:'You Know Better Now Than Ever Before'
+  },
+  {
+    china:'你可能会反对',
+    english:'You May Have Opposition'
+  },
+  {
+    china:'你可能会放弃其他的东西',
+    english:'You May Have To Drop Other Things'
+  },
+  {
+    china:'你必须',
+    english:'You Must'
+  },
+  {
+    china:'你现在必须行动',
+    english:'You Must Act Now'
+  },
+  {
+    china:'你会发现一切你需要知道的',
+    english:'You Will Find Out Everything You\'ll Need To Know'
+  },
+  {
+    china:'你将需要适应',
+    english:'You Will Need To Accommodate'
+  },
+  {
+    china:'你不会失望的',
+    english:'You Will Not Be Disappointed'
+  },
+  {
+    china:'你会很高兴你做了',
+    english:'You\'ll Be Happy You Did'
+  },
+  {
+    china:'你会得到最后的决定',
+    english:'You\'ll Get The Final Word'
+  },
+  {
+    china:'你将不得不妥协',
+    english:'You\'ll Have To Compromise'
+  },
+  {
+    china:'你将不得不补回来',
+    english:'You\'ll Have To Make It Up As You Go'
+  },
+  {
+    china:'您需要了解更多信息',
+    english:'You\'ll Need More Information'
+  },
+  {
+    china:'你需要考虑其他办法',
+    english:'You\'ll Need To Consider Other Ways'
+  },
+  {
+    china:'你需要主动出击',
+    english:'You\'ll Need To Take The Initiative'
+  },
+  {
+    china:'你会后悔的',
+    english:'You\'ll Regret It'
+  },
+  {
+    china:'你的行动将会改善',
+    english:'Your Actions Will Improve Thin'
+  }
+];
+
+var dazs = {
+  zh: [
+    "找个人给你意见",
+    "算了吧",
+    "请教你的妈妈",
+    "当然咯",
+    "谁说得准呢，先观望着",
+    "千万别傻",
+    "保持你的好奇心，去挖掘真相",
+    "把心揣怀里",
+    "答案在镜子里",
+    "不",
+    "这事儿不靠谱",
+    "天上要掉馅饼了",
+    "有好运",
+    "要有耐心",
+    "你需要知道真相",
+    "还有另一种情况",
+    "观望",
+    "别让它影响到你",
+    "是",
+    "信任",
+    "列个清单",
+    "时机不对",
+    "照你想的那样去做",
+    "量力而行",
+    "但行好事，莫问前程",
+    "抛弃首选方案",
+    "走容易走的路",
+    "最佳方案不一定可行",
+    "不会作就不会死",
+    "试试卖萌",
+    "借助他人的经验",
+    "再多考虑",
+    "注意细节",
+    "说出来吧",
+    "不要犹豫",
+    "机会稍纵即逝",
+    "制订了一个新计划",
+    "GO",
+    "谁都不能保证",
+    "情况很快就会发生变化",
+    "不要陷得太深",
+    "转移你的注意力",
+    "至关重要",
+    "告诉自己什么是最重要的",
+    "为什么不",
+    "别傻等了",
+    "不要忘记",
+    "为什么不呢",
+    "去解决",
+    "寻找更多的选择",
+    "上帝为你关一扇门，必定会为你打开一扇窗",
+    "随波逐流未必是好事",
+    "问天问大地，不如问自己",
+    "你就是答案",
+    "去争取机会",
+    "改变不了世界，改变自己",
+    "主动一点，人生会大不相同",
+    "学会妥协",
+    "掌握更多信息",
+    "相信你最初的想法",
+    "勿忘初心，放得始终",
+    "扫除障碍",
+    "把重心放在工作/学习上",
+    "培养一项新的爱好",
+    "对他人慷慨",
+    "不放赌一把",
+    "去做其他的事情",
+    "观察形势",
+    "休息，休息一会",
+    "这是你最后的机会",
+    "再考虑一下",
+    "并不明智",
+    "等待更好的",
+    "很快就能解决",
+    "重要",
+    "是的",
+    "采取行动",
+    "去做",
+    "不要过火",
+    "事情开始变得有趣了",
+    "保存你的实力",
+    "这是一定的",
+    "不确定的因素有点多",
+    "结果不错",
+    "你可能不得不放弃其他东西",
+    "不要犹豫",
+    "不需要",
+    "去倾诉",
+    "告诉别人这对你意味着什么",
+    "无论你做何种选择，结果都是对的",
+    "保持头脑清醒",
+    "制定计划",
+    "很麻烦",
+    "克服困难",
+    "实际一点",
+    "你需要一点帮助",
+    "协作",
+    "需找更多的选择",
+    "负责",
+    "阻止",
+    "你必须现在就行动",
+    "遵守规则",
+    "坚持",
+    "你不会失望的",
+    "需要花费点时间",
+    "不要迫于压力而改变初衷",
+    "显而易见",
+    "不雅忽略身边的人",
+    "抗拒",
+    "不值得斗争",
+    "玩得开心就好",
+    "毋庸置疑",
+    "你也许会失望",
+    "去改变",
+    "一个强有力的承诺会换回更好的结果",
+    "也许有更好的解决方案",
+    "不要害怕",
+    "想法太多，选择太少",
+    "是的",
+    "一笑而过",
+    "取决于你的选择",
+    "随TA去",
+    "你需要考虑其他方面",
+    "一年后就不那么重要了",
+    "醒醒吧，别做梦了",
+    "意义非凡",
+    "默数十秒再问我",
+    "去行动",
+    "发挥你的想象力",
+    "对的",
+    "为了确保最好的结果，保持冷静",
+    "等待",
+    "你必须弥补这个缺点",
+    "你会后悔的",
+    "毫无疑问",
+    "当然",
+    "现在比以往任何时候的情况都要好",
+    "相信你的直觉",
+    "这是一个机会",
+    "去问你爸爸",
+    "从来没有",
+    "寻找一个指路人",
+    "去尝试",
+    "没有",
+    "错的",
+    "别不自量力",
+    "荒谬",
+    "不赌",
+    "不值得冒险",
+    "不妥协",
+    "关注你的家庭生活",
+    "肯定",
+    "不可预测",
+    "绝对不",
+    "我确定",
+    "尽早完成",
+    "令人期待的事情马上要发生",
+    "你需要适应",
+    "表示怀疑",
+    "它会带来好运",
+    "要有耐心",
+    "看看会发生什么",
+    "记录下来",
+    "不宜在这个时候",
+    "决定了就去做",
+    "别要求太多",
+    "放弃第一个方案",
+    "HOLD不住",
+    "谨慎小心",
+    "注意细节",
+    "注意身后",
+    "不要犹豫",
+    "继续前进",
+    "情况很快就会发生改变",
+    "不要被情绪左右",
+    "转移注意力",
+    "着眼未来",
+    "问自己什么是最重要的",
+    "不要等了",
+    "保持乐观",
+    "没有更好的选择",
+    "列出原因",
+    "抓住机会",
+    "改变自己",
+    "你需要主动",
+    "妥协",
+    "有比这更重要的东西",
+    "你需要掌握更多的信息",
+    "删除记忆",
+    "能让你快乐的那个决定",
+    "你需要考虑其他方面",
+    "你会后悔的",
+    "毫无疑问",
+    "当然",
+    "相信自己的直觉",
+    "这是一个机会",
+    "形势不明",
+    "先让自己休息",
+    "重新考虑",
+    "不明智",
+    "抓住机会",
+    "等待机会",
+    "不要做得太过分",
+    "保持现状",
+    "不要忧虑",
+    "有意料之外的事会发生，不妨等待",
+    "你会失望的",
+    "花更多的时间来决定",
+    "你开心就好",
+  ],
+};
